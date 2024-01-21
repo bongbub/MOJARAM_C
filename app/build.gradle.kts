d6058ebe9c681038dbc19690630aa49263755767
bongbub <whgusqls5@naver.com>
2024-01-21 23:52:39 +0900

    로그인 연동 (근데 성공해도 메인화면, 실패해도 메인화면으로 넘어가는 문제 있어서 고쳐야 함) First Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("com.google.firebase:firebase-auth:22.3.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1")) // 로그인구현하며 추가
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
