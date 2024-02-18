098cc439c8afdd41d2db3a97b4d045405169660a
bongbub <whgusqls5@naver.com>
2024-02-19 01:18:33 +0900

    찜페이지 베이스 구현 수정1 -likefragment 페이지와 like.xml연결 - "나의찜목록" text 의 margin value 수정

--- app/src/main/java/com/example/mojaram/likeFragment.kt
+++ app/src/main/java/com/example/mojaram/likeFragment.kt
@@ -1,5 +1,10 @@
 package com.example.mojaram
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 
@@ -10,6 +15,18 @@ class likeFragment : Fragment() {
         fun newInstance() : likeFragment {
             return likeFragment()
         }
+    }
+
+    // 프래그먼트와 레이아웃 연결
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?{
+        Log.d(MeFragment.TAG, "likeFregment - OnCreateView() called")
+
+        val view = inflater.inflate(R.layout.fragment_like, container, false)
+        return view
     }
 
 }
