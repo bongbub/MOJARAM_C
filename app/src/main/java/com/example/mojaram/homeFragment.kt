20c0e96953ab2c36242ef361ec5f125666599bdd
bongbub <whgusqls5@naver.com>
2024-02-26 18:55:23 +0900

    메인화면 tab 추가하였씁니당....진짜올

--- app/src/main/java/com/example/mojaram/homeFragment.kt
+++ app/src/main/java/com/example/mojaram/homeFragment.kt
@@ -8,7 +8,11 @@ import android.view.ViewGroup
 import android.widget.GridLayout
 import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.example.mojaram.ui.login.vrPage
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class homeFragment : Fragment() {
@@ -24,6 +28,29 @@ class homeFragment : Fragment() {
         val banner: LinearLayout = rootView.findViewById(R.id.vrhairEX)
         val gridban: GridLayout = rootView.findViewById(R.id.grid)
 
+        // xml에 있는 요소들
+        val viewPager2 : ViewPager2 = rootView.findViewById(R.id.viewpager2)
+        val tabLayout : TabLayout = rootView.findViewById(R.id.tablayout)
+
+        viewPager2.apply {
+            adapter = MyPageradapter(context as FragmentActivity)
+        }
+
+        TabLayoutMediator(tabLayout, viewPager2) {tab, position->
+            when(position){
+                0->{
+                    tab.text="안양동 추천"
+                }
+                1->{
+                    tab.text="스타일 추천"
+                }
+                else->{
+                    tab.text="홈"
+                }
+
+            }
+        }.attach()
+
         banner.setOnClickListener {
             val intent = Intent(requireContext(), vrPage::class.java)
             startActivity(intent)
@@ -35,6 +62,7 @@ class homeFragment : Fragment() {
         }
         return rootView
     }
+
 
         companion object {
             const val TAG: String = "홈 로그"
