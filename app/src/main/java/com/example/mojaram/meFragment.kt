bdf61b6354cacf9d15d8201b1a67d5838c7def04
bongbub <whgusqls5@naver.com>
2024-03-20 14:18:37 +0900

    마이페이지 강종오류 픽스 못함(우선 주석처리)

--- app/src/main/java/com/example/mojaram/meFragment.kt
+++ app/src/main/java/com/example/mojaram/meFragment.kt
@@ -15,10 +15,10 @@ import com.example.mojaram.databinding.FragmentMyReviewsBinding
 
 class meFragment : Fragment() {
 
-    companion object{
+    companion object {
         const val TAG: String = "마이페이지 로그"
 
-        fun newInstance() : meFragment {
+        fun newInstance(): meFragment {
             return meFragment()
         }
     }
@@ -29,14 +29,14 @@ class meFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View?{
+    ): View? {
         Log.d(TAG, "MeFregment - OnCreateView() called")
 
         val view = inflater.inflate(R.layout.fragment_me, container, false)
 
         // 리뷰 내역
-        val myreviewPage : Button = view.findViewById(R.id.btn_my1)
-        myreviewPage.setOnClickListener{
+        val myreviewPage: Button = view.findViewById(R.id.btn_my1)
+        myreviewPage.setOnClickListener {
             //val intent = Intent(requireContext(), MyReviews::class.java)
             //startActivity(intent)
             // 위의 코드는 activity만 되는 듯
@@ -47,44 +47,45 @@ class meFragment : Fragment() {
             //    .addToBackStack(null)
             //    .commit()
             val fragment = MyReviews.newInstance()
-            addFragment(fragment)
+            //addFragment(fragment)
         }
         fun addFragment(fragment: Fragment) {
-            val transaction = supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragment_container, fragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
+            //val transaction = supportFragmentManager.beginTransaction()
+            //transaction.replace(R.id.fragment_container, fragment)
+            //transaction.addToBackStack(null)
+            //transaction.commit()
 
-        // 예약 내역
-        val reserHis : Button = view.findViewById(R.id.btn_my2)
-        reserHis.setOnClickListener{
-            val intent = Intent(requireContext(), ReservationHistory::class.java)
-            startActivity(intent)
+            // 예약 내역
+            val reserHis: Button = view.findViewById(R.id.btn_my2)
+            reserHis.setOnClickListener {
+                val intent = Intent(requireContext(), ReservationHistory::class.java)
+                startActivity(intent)
+            }
+
+            // 상담 내역
+            val consulhis: Button = view.findViewById(R.id.btn_my3)
+            consulhis.setOnClickListener {
+                val intent = Intent(requireContext(), consultationHistory::class.java)
+                startActivity(intent)
+            }
+
+            // 찜 목록 페이지
+            val bookmark: Button = view.findViewById(R.id.btn_my4)
+            bookmark.setOnClickListener {
+                val intent = Intent(requireContext(), Bookmark::class.java)
+                startActivity(intent)
+            }
+
+            // 환경 설정 페이지
+            val settings: Button = view.findViewById(R.id.btn_my5)
+            settings.setOnClickListener {
+                val intent = Intent(requireContext(), settingsPage::class.java)
+                startActivity(intent)
+            }
+
+
+
         }
-
-        // 상담 내역
-        val consulhis : Button = view.findViewById(R.id.btn_my3)
-        consulhis.setOnClickListener {
-            val intent = Intent(requireContext(), consultationHistory::class.java)
-            startActivity(intent)
-        }
-
-        // 찜 목록 페이지
-        val bookmark : Button = view.findViewById(R.id.btn_my4)
-        bookmark.setOnClickListener{
-            val intent = Intent(requireContext(), Bookmark::class.java)
-            startActivity(intent)
-        }
-
-        // 환경 설정 페이지
-        val settings : Button = view.findViewById(R.id.btn_my5)
-        settings.setOnClickListener{
-            val intent = Intent(requireContext(), settingsPage::class.java)
-            startActivity(intent)
-        }
-
         return view
-
-        }
     }
-
+}
