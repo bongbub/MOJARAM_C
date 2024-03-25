d6f9e28ac37e8f75ba6b8548a95fcb01e63917b0
bongbub <whgusqls5@naver.com>
2024-03-25 19:34:00 +0900

    1. 마이페이지 강종오류 못 고친 상태 2. 회원가입 페이지 강종은 픽스, firestore에 정보 전달은 아직 못 구현 3. 예약페이지 구현중

--- app/src/main/java/com/example/mojaram/Recomm_style.kt
+++ app/src/main/java/com/example/mojaram/Recomm_style.kt
@@ -1,11 +1,14 @@
 package com.example.mojaram
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 
 // 메인페이지 스타일 추천 View 스타일 정보를 담을 Fragment
@@ -21,6 +24,19 @@ class Recomm_style : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_recomm_style, container, false)
+
+        val rootView = inflater.inflate(R.layout.fragment_recomm_style, container, false)
+
+        val styleImg : ImageView = rootView.findViewById(R.id.styleImage)
+        val styleName: TextView = rootView.findViewById(R.id.styleName)
+        val styleDes : TextView = rootView.findViewById(R.id.shop_description)
+        val reserveBtn : Button = rootView.findViewById(R.id.reservation_btn)
+
+        // 예약 페이지로 이동
+        reserveBtn.setOnClickListener{
+            //val intent = Intent(requireContext(),)
+        }
+
     }
 
 }
