815306c478e1c45548d0aebb75b98895bd877f2f
bongbub <whgusqls5@naver.com>
2024-03-29 03:06:07 +0900

    (구린)예약페이지 시간+날짜 완성

--- app/src/main/java/com/example/mojaram/Reservation.kt
+++ app/src/main/java/com/example/mojaram/Reservation.kt
@@ -1,10 +1,16 @@
 package com.example.mojaram
 
+import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.DatePicker
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
 import java.util.Calendar
 
 class Reservation : AppCompatActivity() {
@@ -23,5 +29,35 @@ class Reservation : AppCompatActivity() {
             DatePickerDialog(this, datePickerListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH)).show()
         }
 
+
+        // 시간 선택 다이어로그 만들기
+        // 다이어로그 초기화
+        val dialogBtn: Button = findViewById(R.id.reservetimebtn)
+        val resertime: TextView = findViewById(R.id.resertime)
+        dialogBtn.setOnClickListener{
+            showDialog(resertime)
+        }
+    }
+    // 다이어로그 호출
+    private fun showDialog(resertime:TextView) {
+        // 데이터 담기
+        val timedata : Array<String> = resources.getStringArray(R.array.selectreservetime)
+
+        // AlertDialog 초기화
+        val builder : AlertDialog.Builder = AlertDialog.Builder(this)
+
+        // 제목 설정
+        builder.setTitle("방문 시간")
+
+        // 아이템 선택 이벤트
+        AlertDialog.Builder(this).run{
+            setItems(timedata,object : DialogInterface.OnClickListener{
+                override fun onClick(dialog: DialogInterface?, which: Int) {
+                    resertime.append("${timedata[which]}")
+                }
+            })
+            setNegativeButton("취소",null)
+            show()
+        }
     }
 }
