78a485233547b900230f64366b9d433af564a5ce
sonpyeonghwa <ssrsph@naver.com>
2024-05-29 11:48:05 +0900

    [FEAT] 마이페이지 구현

--- app/src/main/java/com/example/mojaram/MainActivity.kt
+++ app/src/main/java/com/example/mojaram/MainActivity.kt
@@ -12,17 +12,7 @@ import com.example.mojaram.map.MapFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(){
-
-
-    private lateinit var homeFragment: HomeFragment
-    private lateinit var likeFragment: likeFragment
-    private lateinit var mapFragment: MapFragment
-    private lateinit var meFragment: MeFragment
-    private lateinit var hairFragment: HairFragment
-
-
     private lateinit var navController: NavController
-    lateinit var appBarConfiguration: AppBarConfiguration
 
 
     // 메모리에 올라갔을 때(실행)
@@ -30,18 +20,8 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // 네비게이션 컨트롤러와 앱바 컨피규레이션 초고히
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2)as NavHostFragment
         navController = navHostFragment.navController
-
-        // 앱바 설정 객체
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.homeFragment, R.id.meFragment, R.id.hairFragment,
-            R.id.likeFragment, R.id.mapFragment))
-
-        setSupportActionBar((findViewById(R.id.toolbar)))
-
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
         val btn_nav: BottomNavigationView=findViewById(R.id.bottom_navigation)
 
---
+++ app/src/main/java/com/example/mojaram/mypage/MyPageFragment.kt
@@ -0,0 +1,26 @@
+package com.example.mojaram.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mojaram.databinding.FragmentMyPageBinding
+import com.example.mojaram.utils.AutoClearedValue
+
+class MyPageFragment: Fragment() {
+    private var binding by AutoClearedValue<FragmentMyPageBinding>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentMyPageBinding.inflate(inflater, container, false).let {
+        binding = it
+        it.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
