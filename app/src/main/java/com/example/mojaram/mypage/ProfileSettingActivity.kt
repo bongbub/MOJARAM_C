dd252e957930454bdb05dfbc1b2873d61b9fdab8
bongbub <whgusqls5@naver.com>
2024-06-12 20:06:02 +0900

    로그아웃 버튼 구현 (마이페이지 세부 페이지에 있음)

--- app/src/main/java/com/example/mojaram/mypage/ProfileSettingActivity.kt
+++ app/src/main/java/com/example/mojaram/mypage/ProfileSettingActivity.kt
@@ -1,14 +1,23 @@
 package com.example.mojaram.mypage
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.mojaram.LogInActivity
+import com.example.mojaram.R
 import com.example.mojaram.databinding.ActivityProfileSettingBinding
 import com.example.mojaram.utils.HorizontalRecylerViewDivider
 import com.google.android.flexbox.FlexDirection
 import com.google.android.flexbox.FlexWrap
 import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileSettingActivity: AppCompatActivity() {
+    // Firebase auth 사용하기 위해
+    var auth: FirebaseAuth? = null
+
     private lateinit var binding: ActivityProfileSettingBinding
     private val categories = listOf(
         "부분탈모", "남성", "인조모", "인모"
@@ -25,7 +34,23 @@ class ProfileSettingActivity: AppCompatActivity() {
             finish()
         }
         setInterestCategories()
+
+        auth = FirebaseAuth.getInstance()
+
+        val logoutbtn : Button = findViewById(R.id.logoutbtn)
+
+
+        logoutbtn.setOnClickListener {
+            // 로그아웃 버튼을 눌렀을 때 로그아웃
+            FirebaseAuth.getInstance().signOut()
+            val intent = Intent(this, LogInActivity::class.java)
+            Log.d("ProfilesettingActivity","Logout")
+            startActivity(intent)
+            finish()
+        }
     }
+
+
 
     private fun setInterestCategories() {
         binding.recyclerviewCategories.run {
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,10 @@ buildscript {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
         classpath ("com.google.gms:google-services:4.3.15")
     }
+    repositories{
+        google()
+        mavenCentral()
+    }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
