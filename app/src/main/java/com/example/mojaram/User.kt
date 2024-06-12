0aa00c2dbec4dec9806d26c94ad63f97d125b749
bongbub <whgusqls5@naver.com>
2024-06-12 23:10:08 +0900

    dd

--- app/src/main/java/com/example/mojaram/User.kt
+++ app/src/main/java/com/example/mojaram/User.kt
@@ -1,13 +1,14 @@
 package com.example.mojaram
 
 data class User(
-    var name: String? = null,
+    var nickname: String? = null,
     var email: String? = null,
     var password: String? = null,
     // 닉네임(이름) 받고, 생년월일 받기
-    var nickname: String? = null,
-    var birth: String? = null
+    var userType: String? = null,
+    var userGender: String? = null,
+    var userbirth : String? = null
 
 ){
-    constructor():this("","","","","")
+    constructor():this("","","","","","")
 }
---
+++ app/src/main/java/com/example/mojaram/admin/AdminMain.kt
@@ -0,0 +1,12 @@
+package com.example.mojaram.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mojaram.R
+
+class AdminMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_main)
+    }
+}
