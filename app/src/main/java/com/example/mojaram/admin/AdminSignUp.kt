a9ed765648399b88be027113b1d30f0f51568e6c
bongbub <whgusqls5@naver.com>
2024-09-24 02:31:34 +0900

    24.09.24 관리자 회원 등록과 매장 등록 프로세스 재구축 1.SignUpActivity '매장등록하기' 버튼 추가 -> 사용자 정보와 매장 정보를 Firestore 'user_admin'에 한 번에 저장
    2. SignUpActivity 레이아웃 변경
    (1). 전화번호 추가
    (2). 전화번호 양식 적용
    (3). 스크롤뷰로 변경

--- app/src/main/java/com/example/mojaram/admin/AdminSignUp.kt
+++ app/src/main/java/com/example/mojaram/admin/AdminSignUp.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.telephony.PhoneNumberFormattingTextWatcher
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +18,16 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 class AdminSignUp : AppCompatActivity() {
+
+    private lateinit var editTextStoreName: EditText
+    private lateinit var editTextAddress1:EditText
+    private lateinit var editTextAddress2:EditText
+    private lateinit var editTextPhone:EditText
+    private lateinit var editTextProof: EditText
+    private lateinit var editTextHours: EditText
+    private lateinit var editTextPrice: EditText
+    private lateinit var btnAdminSignUp: Button
+
     private lateinit var binding: ActivityAdminSignUpBinding
 
     private val auth = FirebaseAuth.getInstance()
@@ -27,91 +39,87 @@ class AdminSignUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminSignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        // SignUpActivity에서 전달된 정보 받기
+        val nickname = intent.getStringExtra("nickname")
+        val useremail = intent.getStringExtra("useremail")
+        val userGender = intent.getStringExtra("userGender")
+        val userbirth = intent.getStringExtra("userbirth")
+        val password = intent.getStringExtra("password")
 
         binding.buttonSignUp.setOnClickListener{
-            val shop_name = binding.editTextShopname.text.toString()
+            val shop_name = binding.editTextShopname.text.toString().trim()
             val shop_addr = binding.editTextShopaddress.text.toString()
             val shop_addr2 = binding.editTextShopaddress2.text.toString()
-            val shop_num = binding.editTextShopNum.text.toString()
-            val shop_auth = binding.editTextAuth.text.toString()
+            val shop_num = binding.editTextShopNum.text.toString().trim()
+            val shop_auth = binding.editTextAuth.text.toString()      //수정
             val shop_hour = binding.editTextShopHours.text.toString()
             val shop_price = binding.editTextShopPrice.text.toString()
 
-            if (shop_name.isNotEmpty() && shop_addr.isNotEmpty() && shop_addr2.isNotEmpty() && shop_num.isNotEmpty()
-                && shop_auth.isNotEmpty() && shop_hour.isNotEmpty() && shop_price.isNotEmpty()){
-                signUpAdmin(shop_name, shop_addr, shop_addr2, shop_num, shop_auth, shop_hour, shop_price)
-            }else{
-                Toast.makeText(this,"모든 필드를 입력하세요.", Toast.LENGTH_SHORT).show()
+            if (shop_name.isEmpty() || shop_addr.isEmpty() || shop_addr2.isEmpty() || shop_auth.isEmpty() || shop_hour.isEmpty() || shop_price.isEmpty()) {
+                Toast.makeText(this, "모든 필드를 입력해 주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+
+            // 파이어스토어에 저장
+            addAdminToFirestore(nickname, useremail, password, userGender, userbirth, shop_name, shop_addr, shop_addr2, shop_num, shop_auth, shop_hour, shop_price )
+
         }
 
-        // 전화번호 형식 추가
-        binding.editTextShopNum.addTextChangedListener(object : TextWatcher {
-            private var isFormatting: Boolean = false
-            private var prevText: String = ""
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // 저장된 이전 텍스트를 사용해 변화 감지
-                prevText = s.toString()
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // 변환 중 상태를 확인
-                if (isFormatting) return
-            }
-            override fun afterTextChanged(s: Editable?) {
-                if (isFormatting) return
-
-                isFormatting = true
-
-                // 기존 입력 텍스트하이픈 제거
-                val currentText = s.toString().replace("-", "")
-                // 입력된 텍스트 길이 파악 후 하이픈 추가
-                val formattedText = when {
-                    currentText.length > 7 -> "${currentText.substring(0, 3)}-${currentText.substring(3, 7)}-${currentText.substring(7)}"
-                    currentText.length > 3 -> "${currentText.substring(0, 3)}-${currentText.substring(3)}"
-                    else -> currentText
-                }
-                // 업데이트 시 이미 있는 텍스트의 커서 위치 조정
-                if (formattedText != prevText) {
-                    binding.editTextShopNum.setText(formattedText)
-                    binding.editTextShopNum.setSelection(formattedText.length)
-                }
-
-                isFormatting = false
-            }
-        })
-
-        binding.pricePlusbtn.setOnClickListener{
-
-        }
+//        // 전화번호 형식 추가
+//        binding.editTextShopNum.addTextChangedListener(object : TextWatcher {
+//            private var isFormatting: Boolean = false
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+//            override fun afterTextChanged(s: Editable?) {
+//                if (isFormatting) return
+//
+//                isFormatting = true
+//
+//                val currentText = s.toString().replace("-", "")
+//                val formattedText = when {
+//                    currentText.length > 7 -> "${currentText.substring(0, 3)}-${currentText.substring(3, 7)}-${currentText.substring(7)}"
+//                    currentText.length > 3 -> "${currentText.substring(0, 3)}-${currentText.substring(3)}"
+//                    else -> currentText
+//                }
+//
+//                binding.editTextShopNum.setText(formattedText)
+//                binding.editTextShopNum.setSelection(formattedText.length)
+//
+//                isFormatting = false
+//            }
+//        })
     }
 
-    private fun signUpAdmin(
-        shop_name: String, shop_addr: String, shop_addr2:String, shop_num:String, shop_auth:String, shop_hour:String, shop_price:String){
+    private fun addAdminToFirestore(
+        nickname: String?, useremail: String?, password:String?, userGender:String?, userbirth:String?,
+        shop_name:String?,shop_addr:String?,shop_addr2:String?,shop_num:String?,
+        shop_auth:String?,shop_hour:String?,shop_price:String?){
+        val adminData = mapOf(
+            "nickname" to nickname,
+            "useremail" to useremail,
+            "password" to password,
+            "userGender" to userGender,
+            "userbirth" to userbirth,
+            "shop_name" to shop_name,
+            "shop_addr" to shop_addr,
+            "shop_addr2" to shop_addr2,
+            "shop_num" to shop_num,
+            "shop_auth" to shop_auth,
+            "shop_hour" to shop_hour,
+            "shop_price" to shop_price,
+            "role" to "001"
+        )
 
-        val user = auth.currentUser
-        if(user != null){
-            val email = user.email ?: return
-            val adminData = hashMapOf(
-                "shopName" to shop_name,
-                "shopAddr" to shop_addr,
-                "shopAddr2" to shop_addr2,
-                "shopNum" to shop_num,
-                "shopAuth" to shop_auth,
-                "shopHour" to shop_hour,
-                "shopPrice" to shop_price
-            )
-
-            db.collection("user_admin").document(email).set(adminData)
-                .addOnSuccessListener {
-                    Toast.makeText(this,"제출이 완료되었습니다.", Toast.LENGTH_SHORT).show()
-                    finish()
-                }
-                .addOnFailureListener{ e ->
-                    Toast.makeText(this,"제출이 실패하였습니다 : ${e.message}", Toast.LENGTH_SHORT).show()
-                }
-        }else{
-            Toast.makeText(this,"로그인 정보가 없습니다", Toast.LENGTH_SHORT).show()
-        }
+        db.collection("user_admin").document(useremail!!).set(adminData)
+            .addOnSuccessListener {
+                Toast.makeText(this,"요청이 완료되었습니다.",Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .addOnFailureListener{ e ->
+                Toast.makeText(this,"요청 실패 : ${e.message}", Toast.LENGTH_SHORT).show()
+                Log.w("Firestore","파이어스토어에 관리자 정보 업로드 오류",e)
+            }
     }
 }
--- app/src/main/java/com/example/mojaram/ui/login/Signup.kt
+++ app/src/main/java/com/example/mojaram/ui/login/Signup.kt
@@ -18,6 +18,9 @@ import com.example.mojaram.R
 import com.example.mojaram.LogInActivity
 import com.example.mojaram.User
 import com.example.mojaram.admin.AdminMain
+import com.example.mojaram.admin.AdminSignUp
+import com.example.mojaram.databinding.ActivityAdminSignUpBinding
+import com.example.mojaram.databinding.ActivitySignupBinding
 // Firebase 연동을 위한 import
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
@@ -33,7 +36,9 @@ class SignUpActivity : AppCompatActivity() {
     private lateinit var editTextid: EditText
     private lateinit var editTextPassword: EditText
     private lateinit var btnSignUp: Button
-    // 생년월일
+    private lateinit var btnShopRegi: Button
+    private lateinit var editTextnumber: EditText
+
     private lateinit var Userbirthday : EditText
     
     // 비밀번호 체크
@@ -51,7 +56,11 @@ class SignUpActivity : AppCompatActivity() {
     // Firebase Authentication 연동을 위한 객체
     private lateinit var mAuth: FirebaseAuth
 
+    private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
@@ -63,6 +72,8 @@ class SignUpActivity : AppCompatActivity() {
         textview_passcheck = findViewById(R.id.textview_passcheck)
         editTextpassword_check = findViewById(R.id.editTextPassword_check)
         btnSignUp = findViewById(R.id.btnSignUp)
+        btnShopRegi = findViewById(R.id.btnShopRegi)
+        editTextnumber = findViewById(R.id.numberText)
 
         // 생년월일 가져오기
         Userbirthday = findViewById(R.id.birthText)
@@ -93,24 +104,88 @@ class SignUpActivity : AppCompatActivity() {
             val password = editTextPassword.text.toString().trim()
             val passwordCheck = textview_passcheck.text.toString().trim()
             val userbirth = Userbirthday.text.toString().trim()
+            val editTextnumber = editTextnumber.text.toString().trim()
 
 
             // 성별 판별
             val userGender = if(checkMale.isChecked) "male" else "female"
 
 
-            if (nickname.isEmpty() || useremail.isEmpty() || password.isEmpty() || userbirth.isEmpty()) {
+            if (nickname.isEmpty() || useremail.isEmpty() || password.isEmpty() || userbirth.isEmpty() || password.isEmpty() || editTextnumber.isEmpty()) {
                 Toast.makeText(this, "모든 필드를 입력해 주세요.", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
+            }else if (passwordCheck.isEmpty()){
+                Toast.makeText(this, "비밀번호 재입력을 확인해주세요",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+
             // 회원가입 정보 Firebase RealtimeDatabase 연동
-            Register(nickname, useremail, password, userGender, userbirth) //여기 순서 중요
+            Register(nickname, useremail, password, editTextnumber,userGender, userbirth) //여기 순서 중요
         }
+
+        // 매장 등록 버튼을 눌렀을 때
+        btnShopRegi.setOnClickListener{
+
+            val nickname = editTextUsername.text.toString().trim()
+            val useremail = editTextid.text.toString().trim()
+            val password = editTextPassword.text.toString().trim()
+            val passwordCheck = textview_passcheck.text.toString().trim()
+            val userbirth = Userbirthday.text.toString().trim()
+            val userGender = if(checkMale.isChecked) "male" else "female"
+
+
+            if (nickname.isEmpty() || useremail.isEmpty() || password.isEmpty() || userbirth.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "모든 필드를 입력해 주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }else if (passwordCheck.isEmpty()){
+                Toast.makeText(this, "비밀번호 재입력을 확인해주세요",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+            // 기존 입력한 정보를 갖고 매장 등록 페이지로 전환
+            val intent = Intent(this, AdminSignUp::class.java).apply {
+                putExtra("nickname", editTextUsername.text.toString().trim())
+                putExtra("useremail",editTextid.text.toString().trim())
+                putExtra("userGender",if(checkMale.isChecked)"male" else "female")
+                putExtra("userbirth", Userbirthday.text.toString().trim())
+                putExtra("password", editTextPassword.text.toString().trim())
+                putExtra("number", editTextnumber.text.toString().trim())
+            }
+            startActivity(intent)
+        }
+
+        // 전화번호 형식
+        binding.numberText.addTextChangedListener(object : TextWatcher {
+            private var isFormatting: Boolean = false
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (isFormatting) return
+
+                isFormatting = true
+
+                val currentText = s.toString().replace("-", "")
+                val formattedText = when {
+                    currentText.length > 7 -> "${currentText.substring(0, 3)}-${currentText.substring(3, 7)}-${currentText.substring(7)}"
+                    currentText.length > 3 -> "${currentText.substring(0, 3)}-${currentText.substring(3)}"
+                    else -> currentText
+                }
+
+                binding.numberText.setText(formattedText)
+                binding.numberText.setSelection(formattedText.length)
+
+                isFormatting = false
+            }
+        })
+
+
     }
 
 
     //firebase Register 연동
-    private fun Register(nickname: String, useremail:String, password: String, userGender: String, userbirth: String) {
+    private fun Register(nickname: String, useremail:String, password: String, editTextnumber:String, userGender: String, userbirth: String) {
 
         mAuth.createUserWithEmailAndPassword(useremail, password)
             .addOnCompleteListener(this) { task ->
@@ -126,7 +201,7 @@ class SignUpActivity : AppCompatActivity() {
 
                     // User Class에 데이터를 담아서
                     // Firestore로 전송
-                    addUserToFirestore(nickname, useremail, password, userGender, userbirth, mAuth.currentUser?.uid!!)
+                    addUserToFirestore(nickname, useremail, password, editTextnumber, userGender, userbirth, mAuth.currentUser?.uid!!)
                 } else {
                     val errorMessage = task.exception?.message ?: "회원가입에 실패하였습니다. 다시 시도해주세요. 비밀번호는 6자 이상이어야 합니다."
                     Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
@@ -134,8 +209,8 @@ class SignUpActivity : AppCompatActivity() {
                 }
             }
     }
-    private fun addUserToFirestore(nickname: String, useremail: String, password: String, userGender: String, userbirth: String, uId: String){
-        val user = User(useremail, nickname,  password, userGender,userbirth, uId)
+    private fun addUserToFirestore(nickname: String, useremail: String, password: String, editTextnumber: String, userGender: String, userbirth: String, uId: String){
+        val user = User(useremail, nickname,  password,editTextnumber, userGender,userbirth, uId)
         db.collection("user_customer").document(useremail).set(user)
             .addOnSuccessListener {
                 Log.d("Firestore","파이어스토어 회원 정보 업로드 완료")
